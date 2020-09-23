5a5370c7b4f13ad99921ecd2f026397078e6101c
Flavio Trindade <fhtd04@gmail.com>
2020-09-23 12:37:44 -0300

    controller registrations

--- db/migrate/20200923134452_add_fields_to_user.rb
+++ db/migrate/20200923134452_add_fields_to_user.rb
@@ -1,7 +1,7 @@
 class AddFieldsToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :admin, :boolean
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
+    add_column :users, :admin, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -42,9 +42,9 @@ ActiveRecord::Schema.define(version: 2020_09_23_140805) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.boolean "admin"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
