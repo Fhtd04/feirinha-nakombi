b83f68eef831f1e88503882f21e402bc5755b466
Flavio Trindade <fhtd04@gmail.com>
2020-09-24 16:39:10 -0300

    photo presence

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ApplicationRecord
   has_many :orders
   has_one_attached :photo
 
-  validates :name, :quantity, :price, presence: true
+  validates :name, :quantity, :price, :photo, presence: true
   validates :price, :quantity, numericality: { only_integer: true }
 end
