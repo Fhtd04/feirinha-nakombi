d79556c6588e6d1d7b1707d7ca9241a0d094b88a
Antonio Dantas <antoniocrdantas@gmail.com>
2020-09-23 23:26:31 -0300

    Products views updated with CSS, buttons  and Carousel; Some buggs fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,7 +21,7 @@ class ProductsController < ApplicationController
     @product.user = current_user
 
     if @product.save
-      redirect_to @product, notice: "Product was created!"
+      redirect_to @product, notice: "Produto criado!"
     else
       render :new
     end
@@ -29,22 +29,22 @@ class ProductsController < ApplicationController
 
   def update
     if @product.user != current_user
-      redirect_to products_path, alert: "Not authorized!"
+      redirect_to products_path, alert: "Você não tem autorização!"
       return
     end
 
     @product.update(product_params)
-    redirect_to @product, notice: "Product was updated!"
+    redirect_to @product, notice: "Produto atualizado!"
   end
 
   def destroy
     if @product.user != current_user
-      redirect_to root_path, alert: "Not authorized!"
+      redirect_to root_path, alert: "Você não tem autorização!"
       return
     end
 
     @product.destroy
-    redirect_to products_url, notice: "Restaurant was destroyed!"
+    redirect_to products_url, notice: "Produto deletado!"
   end
 
   private
